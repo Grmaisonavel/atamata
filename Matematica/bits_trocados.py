3bed883e8efa691e8fe4cb1330b69af935101134
Grmaisonavel <gabriel.maisonavel@gmail.com>
2024-09-26 07:37:37 -0300

    Update bits_trocados.py

--- Matematica/bits_trocados.py
+++ Matematica/bits_trocados.py
@@ -4,8 +4,10 @@ k = 0
 l = 0
 valor = [i,j,k,l]
 
+#Recebe o valor a ser retirado
 v = int(input("Insira o valor que deseja retirar: "))
 
+#Vê quantas notas de cada tipos são necessárias para atingir o valor
 if v >= 50:
     i = v//50
     v = v-i*50
@@ -22,4 +24,5 @@ if v >= 1:
     l = v//1
     v = v-l*1
 
+#Mostra a quantidade de notas necessárias de cada valor
 print(i,j,k,l)
